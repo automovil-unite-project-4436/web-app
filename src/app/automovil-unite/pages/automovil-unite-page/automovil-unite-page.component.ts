3dc706bb81b0d71661de3ec8b602284d2d674a2b
Loberos <rengiforandy11@gmail.com>
2025-04-10 19:04:09 -0500

    a

--- src/app/automovil-unite/pages/automovil-unite-page/automovil-unite-page.component.ts
+++ src/app/automovil-unite/pages/automovil-unite-page/automovil-unite-page.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-=======
-import {RouterOutlet} from '@angular/router';
->>>>>>> 00ed5043498fcca120f63ac1aea0e576fb117a48
+
 import {SidebarComponent} from '../../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-automovil-unite-page',
   imports: [
     SidebarComponent
-<<<<<<< HEAD
-
-=======
->>>>>>> 00ed5043498fcca120f63ac1aea0e576fb117a48
   ],
   templateUrl: './automovil-unite-page.component.html',
   styleUrl: './automovil-unite-page.component.css'
